b1230769ec41579bd06032c5fd3815b252b578de
62sorata <107105246+62sorata@users.noreply.github.com>
2023-02-27 16:26:59 +0900

    新しいオブジェクトストアの作成

--- addMemu.js
+++ addMemu.js
@@ -1,4 +1,16 @@
+var count = 0;
+
 function addTage() {
+    count++;
+    var newTag = document.getElementById("tag").value;  //新しいタグを格納
+    var transaction = db.transaction(["tagstore"], "readwrite");  //DBへ書き込み
+	var store = transaction.objectStore("tagstore");  
+    var request = store.put({ tagKey: count, tagvalue: newTag});  //０～のcountの値をKEYにTagを配列のように格納
+
+    request.onsuccess = function (event) {
+        document.getElementById("tag").value = "";
+    }
+
     var newElement = document.createElement("a"); //a要素の作成
     var newContent = document.createTextNode("#");  //タグの内容
     newElement.appendChild(newContent);  //a要素にテキスト追加
